978feb3af0b998fefe75b248da26b67e4f728f4d
ADARSH CHOUKSEY <choukseyaryan49@gmail.com>
2025-03-27 12:57:12 +0530

    buy and sell stock 2

--- edit_distance_33.cpp
+++ edit_distance_33.cpp
@@ -6,7 +6,7 @@ class Solution {
         if(i<0)return j+1;
         if(j<0)return i+1;
         if(dp[i][j]!=-1)return dp[i][j];
-        if(s[i]==t[j]){
+        if(s[i]==t[j]){ 
             return f(i-1,j-1,s,t,dp);
         }
         return dp[i][j]=(1+min(f(i-1,j,s,t,dp),min(f(i,j-1,s,t,dp),f(i-1,j-1,s,t,dp))));
